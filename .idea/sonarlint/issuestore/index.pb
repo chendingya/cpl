eafde5701b93ea308e460b1419025e662e9d775c
chendingya <211250094@smail.nju.edu.cn>
2021-12-26 13:37:33 +0800

    12.25

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+?
+0-intro/hello.c,1\d\1dd5e8179e8fcea59f5482e42f0e5693a41a67c1
--- 10-assignments/linkedlist.c
+++ 10-assignments/linkedlist.c
@@ -8,33 +8,64 @@
 typedef struct node {
     int value;
     struct node *next;
+    struct node *prev;
 }Node;
 typedef struct ll {
     Node *head;
     Node *tail;
-} Linkedlist ;
+} Linkedlist;
 
 //function
-void ADD(Linkedlist list, int i);
-void PRINT(Linkedlist *list, int cur);
+void ADD(Linkedlist *list, int i);
+void Forward( int num);
+void Backward(int num);
+void Insert(int num);
+void Remove();
+void Print();
 void Init(Linkedlist *list);
+int JudgeOrder();
+
+//variable
+char order[10];
+Node *ptr;
+Node *pre;
+int count;
+
 //main
-
-
 int main (int argc, char *argv[]) {
     int n,t;
+    int num;
     scanf("%d%d\n", &n, &t);
     //init
     Linkedlist list;//only contains Node *head / Node *tail
     Init(&list);
     //append
     for (int i = 1; i <= n; i++) {
-        ADD(list, i);
+        ADD(&list, i);
     }
-    //print to check
-    for (i = first; i != NULL; i = i->next) {
-        printf("%d\t", i->value);
+    //pointers
+
+    ptr = (list.head);
+    count = n;
+
+    //assess
+    for (int i = 0; i < t; i++) {
+        scanf("%s", &order);
+
+        int judge = JudgeOrder(order);
+        switch (judge) {
+            case 1: scanf("%d", &num);Forward(num);break;
+            case 2: scanf("%d", &num);Backward(num);break;
+            case 3: scanf("%d", &num);Insert(num);count++;break;
+            case 4: Remove();count--;break;
+            case 5: Print();
+
+                break;
+            default:
+                printf("Error default\n"); break;
+        }
     }
+
 
     return 0;
 }
@@ -54,6 +85,7 @@ void ADD(Linkedlist *list, int i) {
     node->value = i;
     //首尾相连
     node->next = list->head;
+    node->prev = list->tail;
 
     if (list->tail == NULL) {
         //链表中没有节点
@@ -64,8 +96,54 @@ void ADD(Linkedlist *list, int i) {
     }
     //调整尾指针
     list->tail = node;
+    list->head->prev = node;
 }
-void PRINT(Linkedlist *list, int cur) {
+int JudgeOrder() {
+    if (order[0] == 'F') {
+        return 1;
+    }
+    if (order[0] == 'B') {
+        return 2;
+    }
+    if (order[0] == 'I') {
+        return 3;
+    }
+    if (order[0] == 'R') {
+        return 4;
+    }
+    if (order[0] == 'P') {
+        return 5;
+    }
+    return 6;
+}
+void Forward(int num) {
+    int tmp = num % count;
+    for (int i = 0; i < tmp; i++) {
+        ptr = (*ptr).next;
+    }
+}
+void Backward(int num) {
+    int tmp = num % count;
+    for (int i = 0; i < tmp; i++) {
+        ptr = ptr->prev;
+    }
+}
+void Insert(int num) {
+    Node *node = malloc(sizeof (*node));
+    node->value = num;
+    node->prev = ptr;
+    node->next = (*ptr).next;
+    ptr->next->prev = node;
+    (*ptr).next = node;
+}
+void Remove() {
+    ptr->prev->next = ptr->next;
+    ptr->next->prev = ptr->prev;
+    ptr = ptr->next;
 
+}
+void Print() {
+    int tmp = (*ptr).value;
+    printf("%d\n", tmp);
 }
 
